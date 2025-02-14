dc288ff0651c92e0e8ef22653ad7f2287c7a7a80
Mathis Brossier <mathis.brossier@gmail.com>
2025-02-14 10:41:06 +0100

    fetch wesl-testsuite

--- crates/wesl-test/wesl-testsuite/fetch.sh
+++ crates/wesl-test/wesl-testsuite/fetch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_url="https://raw.githubusercontent.com/wgsl-tooling-wg/wesl-testsuite/main/src/test-cases-json/"
-files=("importCases.json" "importSyntaxCases.json" "ctsParseTests.json")
+files=("bulkTests.json" "importCases.json" "importSyntaxCases.json")
 
 for file in "${files[@]}"; do
   curl -O "${base_url}${file}"
