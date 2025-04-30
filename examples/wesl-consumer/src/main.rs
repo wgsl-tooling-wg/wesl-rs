5bc12426e747802be3cb68696f712942a9d3e52e
Mathis Brossier <mathis.brossier@gmail.com>
2025-04-30 20:49:47 +0200

    started working on quote_wesl!

--- examples/wesl-consumer/src/main.rs
+++ examples/wesl-consumer/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
+    // run wesl at build-time
     #[cfg(feature = "build-time")]
     let source = {
         use wesl::include_wesl;
         include_wesl!("main")
     };
 
+    // run wesl at run-time
     #[cfg(not(feature = "build-time"))]
     let source = wesl::Wesl::new("src/shaders")
         .add_package(&random_wgsl::random::Mod)
@@ -17,4 +19,17 @@ fn main() {
         .to_string();
 
     println!("{source}");
+
+    // using the procedural macro
+    let source = {
+        use wesl::quote_wesl;
+        quote_wesl! {
+            import bevy::pbr::{ foo };
+
+            fn main() {
+                return foo();
+            }
+        }
+    };
+    println!("quoted: {source}")
 }
