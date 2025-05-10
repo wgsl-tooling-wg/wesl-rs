29f859b46f0b2eb1551de5e41c6604d3c941056e
Mathis Brossier <mathis.brossier@gmail.com>
2025-05-10 17:53:57 +0200

    use builtin idents with injection in new prelude, allow multiple injections with to_owned/into

--- crates/wesl/src/eval/builtin.rs
+++ crates/wesl/src/eval/builtin.rs
@@ -155,9 +155,12 @@ pub static ATTR_INTRINSIC: LazyLock<Attribute> = LazyLock::new(|| {
 });
 
 pub static PRELUDE: LazyLock<TranslationUnit> = LazyLock::new(|| {
+    let intrinsic: &Attribute = &ATTR_INTRINSIC;
+    let abstract_int = builtin_ident("__AbstractInt").unwrap();
+    let abstract_float = builtin_ident("__AbstractFloat").unwrap();
     quote_module! {
         // The prelude contains all pre-declared aliases, built-in structs and functions in WGSL.
-        // the @__intrinsic attribute indicates that a function definition is defined by the compiler.
+        // the @#intrinsic attribute indicates that a function definition is defined by the compiler.
         // it means that it is not representable with user code: has generics, or variadics.
 
         alias vec2i = vec2<i32>;
@@ -197,197 +200,197 @@ pub static PRELUDE: LazyLock<TranslationUnit> = LazyLock::new(|| {
         // therefore it avoids name collisions. AbstractInt and AbstractFloat too.
         struct __frexp_result_f32 { fract: f32, exp: i32 }
         struct __frexp_result_f16 { fract: f16, exp: i32 }
-        struct __frexp_result_abstract { fract: __AbstractFloat, exp: __AbstractInt }
+        struct __frexp_result_abstract { fract: #abstract_float, exp: #abstract_int }
         struct __frexp_result_vec2_f32 { fract: vec2<f32>, exp: vec2<i32> }
         struct __frexp_result_vec3_f32 { fract: vec3<f32>, exp: vec3<i32> }
         struct __frexp_result_vec4_f32 { fract: vec4<f32>, exp: vec4<i32> }
         struct __frexp_result_vec2_f16 { fract: vec2<f16>, exp: vec2<i32> }
         struct __frexp_result_vec3_f16 { fract: vec3<f16>, exp: vec3<i32> }
         struct __frexp_result_vec4_f16 { fract: vec4<f16>, exp: vec4<i32> }
-        struct __frexp_result_vec2_abstract { fract: vec2<__AbstractFloat>, exp: vec2<__AbstractInt> }
-        struct __frexp_result_vec3_abstract { fract: vec3<__AbstractFloat>, exp: vec3<__AbstractInt> }
-        struct __frexp_result_vec4_abstract { fract: vec4<__AbstractFloat>, exp: vec4<__AbstractInt> }
+        struct __frexp_result_vec2_abstract { fract: vec2<#abstract_float>, exp: vec2<#abstract_int> }
+        struct __frexp_result_vec3_abstract { fract: vec3<#abstract_float>, exp: vec3<#abstract_int> }
+        struct __frexp_result_vec4_abstract { fract: vec4<#abstract_float>, exp: vec4<#abstract_int> }
         struct __modf_result_f32 { fract: f32, whole: f32 }
         struct __modf_result_f16 { fract: f16, whole: f16 }
-        struct __modf_result_abstract { fract: __AbstractFloat, whole: __AbstractFloat }
+        struct __modf_result_abstract { fract: #abstract_float, whole: #abstract_float }
         struct __modf_result_vec2_f32 { fract: vec2<f32>, whole: vec2<f32> }
         struct __modf_result_vec3_f32 { fract: vec3<f32>, whole: vec3<f32> }
         struct __modf_result_vec4_f32 { fract: vec4<f32>, whole: vec4<f32> }
         struct __modf_result_vec2_f16 { fract: vec2<f16>, whole: vec2<f16> }
         struct __modf_result_vec3_f16 { fract: vec3<f16>, whole: vec3<f16> }
         struct __modf_result_vec4_f16 { fract: vec4<f16>, whole: vec4<f16> }
-        struct __modf_result_vec2_abstract { fract: vec2<__AbstractFloat>, whole: vec2<__AbstractFloat> }
-        struct __modf_result_vec3_abstract { fract: vec3<__AbstractFloat>, whole: vec3<__AbstractFloat> }
-        struct __modf_result_vec4_abstract { fract: vec4<__AbstractFloat>, whole: vec4<__AbstractFloat> }
+        struct __modf_result_vec2_abstract { fract: vec2<#abstract_float>, whole: vec2<#abstract_float> }
+        struct __modf_result_vec3_abstract { fract: vec3<#abstract_float>, whole: vec3<#abstract_float> }
+        struct __modf_result_vec4_abstract { fract: vec4<#abstract_float>, whole: vec4<#abstract_float> }
         @generic(T) struct atomic_compare_exchange_result { old_value: T, exchanged: bool }
 
         // bitcast
-        @const @must_use fn bitcast() @__intrinsic {}
+        @const @must_use fn bitcast() @#intrinsic {}
 
         // logical
-        @const @must_use fn all() @__intrinsic {}
-        @const @must_use fn any() @__intrinsic {}
-        @const @must_use fn select() @__intrinsic {}
+        @const @must_use fn all() @#intrinsic {}
+        @const @must_use fn any() @#intrinsic {}
+        @const @must_use fn select() @#intrinsic {}
 
         // array
-        @const @must_use fn arrayLength() @__intrinsic {}
+        @const @must_use fn arrayLength() @#intrinsic {}
 
         // numeric
-        @const @must_use fn abs() @__intrinsic {}
-        @const @must_use fn acos() @__intrinsic {}
-        @const @must_use fn acosh() @__intrinsic {}
-        @const @must_use fn asin() @__intrinsic {}
-        @const @must_use fn asinh() @__intrinsic {}
-        @const @must_use fn atan() @__intrinsic {}
-        @const @must_use fn atanh() @__intrinsic {}
-        @const @must_use fn atan2() @__intrinsic {}
-        @const @must_use fn ceil() @__intrinsic {}
-        @const @must_use fn clamp() @__intrinsic {}
-        @const @must_use fn cos() @__intrinsic {}
-        @const @must_use fn cosh() @__intrinsic {}
-        @const @must_use fn countLeadingZeros() @__intrinsic {}
-        @const @must_use fn countOneBits() @__intrinsic {}
-        @const @must_use fn countTrailingZeros() @__intrinsic {}
-        @const @must_use fn cross() @__intrinsic {}
-        @const @must_use fn degrees() @__intrinsic {}
-        @const @must_use fn determinant() @__intrinsic {}
-        @const @must_use fn distance() @__intrinsic {}
-        @const @must_use fn dot() @__intrinsic {}
-        @const @must_use fn dot4U8Packed() @__intrinsic {}
-        @const @must_use fn dot4I8Packed() @__intrinsic {}
-        @const @must_use fn exp() @__intrinsic {}
-        @const @must_use fn exp2() @__intrinsic {}
-        @const @must_use fn extractBits() @__intrinsic {}
-        @const @must_use fn faceForward() @__intrinsic {}
-        @const @must_use fn firstLeadingBit() @__intrinsic {}
-        @const @must_use fn firstTrailingBit() @__intrinsic {}
-        @const @must_use fn floor() @__intrinsic {}
-        @const @must_use fn fma() @__intrinsic {}
-        @const @must_use fn fract() @__intrinsic {}
-        @const @must_use fn frexp() @__intrinsic {}
-        @const @must_use fn insertBits() @__intrinsic {}
-        @const @must_use fn inverseSqrt() @__intrinsic {}
-        @const @must_use fn ldexp() @__intrinsic {}
-        @const @must_use fn length() @__intrinsic {}
-        @const @must_use fn log() @__intrinsic {}
-        @const @must_use fn log2() @__intrinsic {}
-        @const @must_use fn max() @__intrinsic {}
-        @const @must_use fn min() @__intrinsic {}
-        @const @must_use fn mix() @__intrinsic {}
-        @const @must_use fn modf() @__intrinsic {}
-        @const @must_use fn normalize() @__intrinsic {}
-        @const @must_use fn pow() @__intrinsic {}
-        @const @must_use fn quantizeToF16() @__intrinsic {}
-        @const @must_use fn radians() @__intrinsic {}
-        @const @must_use fn reflect() @__intrinsic {}
-        @const @must_use fn refract() @__intrinsic {}
-        @const @must_use fn reverseBits() @__intrinsic {}
-        @const @must_use fn round() @__intrinsic {}
-        @const @must_use fn saturate() @__intrinsic {}
-        @const @must_use fn sign() @__intrinsic {}
-        @const @must_use fn sin() @__intrinsic {}
-        @const @must_use fn sinh() @__intrinsic {}
-        @const @must_use fn smoothstep() @__intrinsic {}
-        @const @must_use fn sqrt() @__intrinsic {}
-        @const @must_use fn step() @__intrinsic {}
-        @const @must_use fn tan() @__intrinsic {}
-        @const @must_use fn tanh() @__intrinsic {}
-        @const @must_use fn transpose() @__intrinsic {}
-        @const @must_use fn trunc() @__intrinsic {}
+        @const @must_use fn abs() @#intrinsic {}
+        @const @must_use fn acos() @#intrinsic {}
+        @const @must_use fn acosh() @#intrinsic {}
+        @const @must_use fn asin() @#intrinsic {}
+        @const @must_use fn asinh() @#intrinsic {}
+        @const @must_use fn atan() @#intrinsic {}
+        @const @must_use fn atanh() @#intrinsic {}
+        @const @must_use fn atan2() @#intrinsic {}
+        @const @must_use fn ceil() @#intrinsic {}
+        @const @must_use fn clamp() @#intrinsic {}
+        @const @must_use fn cos() @#intrinsic {}
+        @const @must_use fn cosh() @#intrinsic {}
+        @const @must_use fn countLeadingZeros() @#intrinsic {}
+        @const @must_use fn countOneBits() @#intrinsic {}
+        @const @must_use fn countTrailingZeros() @#intrinsic {}
+        @const @must_use fn cross() @#intrinsic {}
+        @const @must_use fn degrees() @#intrinsic {}
+        @const @must_use fn determinant() @#intrinsic {}
+        @const @must_use fn distance() @#intrinsic {}
+        @const @must_use fn dot() @#intrinsic {}
+        @const @must_use fn dot4U8Packed() @#intrinsic {}
+        @const @must_use fn dot4I8Packed() @#intrinsic {}
+        @const @must_use fn exp() @#intrinsic {}
+        @const @must_use fn exp2() @#intrinsic {}
+        @const @must_use fn extractBits() @#intrinsic {}
+        @const @must_use fn faceForward() @#intrinsic {}
+        @const @must_use fn firstLeadingBit() @#intrinsic {}
+        @const @must_use fn firstTrailingBit() @#intrinsic {}
+        @const @must_use fn floor() @#intrinsic {}
+        @const @must_use fn fma() @#intrinsic {}
+        @const @must_use fn fract() @#intrinsic {}
+        @const @must_use fn frexp() @#intrinsic {}
+        @const @must_use fn insertBits() @#intrinsic {}
+        @const @must_use fn inverseSqrt() @#intrinsic {}
+        @const @must_use fn ldexp() @#intrinsic {}
+        @const @must_use fn length() @#intrinsic {}
+        @const @must_use fn log() @#intrinsic {}
+        @const @must_use fn log2() @#intrinsic {}
+        @const @must_use fn max() @#intrinsic {}
+        @const @must_use fn min() @#intrinsic {}
+        @const @must_use fn mix() @#intrinsic {}
+        @const @must_use fn modf() @#intrinsic {}
+        @const @must_use fn normalize() @#intrinsic {}
+        @const @must_use fn pow() @#intrinsic {}
+        @const @must_use fn quantizeToF16() @#intrinsic {}
+        @const @must_use fn radians() @#intrinsic {}
+        @const @must_use fn reflect() @#intrinsic {}
+        @const @must_use fn refract() @#intrinsic {}
+        @const @must_use fn reverseBits() @#intrinsic {}
+        @const @must_use fn round() @#intrinsic {}
+        @const @must_use fn saturate() @#intrinsic {}
+        @const @must_use fn sign() @#intrinsic {}
+        @const @must_use fn sin() @#intrinsic {}
+        @const @must_use fn sinh() @#intrinsic {}
+        @const @must_use fn smoothstep() @#intrinsic {}
+        @const @must_use fn sqrt() @#intrinsic {}
+        @const @must_use fn step() @#intrinsic {}
+        @const @must_use fn tan() @#intrinsic {}
+        @const @must_use fn tanh() @#intrinsic {}
+        @const @must_use fn transpose() @#intrinsic {}
+        @const @must_use fn trunc() @#intrinsic {}
 
         // derivative
-        @must_use fn dpdx() @__intrinsic {}
-        @must_use fn dpdxCoarse() @__intrinsic {}
-        @must_use fn dpdxFine() @__intrinsic {}
-        @must_use fn dpdy() @__intrinsic {}
-        @must_use fn dpdyCoarse() @__intrinsic {}
-        @must_use fn dpdyFine() @__intrinsic {}
-        @must_use fn fwidth() @__intrinsic {}
-        @must_use fn fwidthCoarse() @__intrinsic {}
-        @must_use fn fwidthFine() @__intrinsic {}
+        @must_use fn dpdx() @#intrinsic {}
+        @must_use fn dpdxCoarse() @#intrinsic {}
+        @must_use fn dpdxFine() @#intrinsic {}
+        @must_use fn dpdy() @#intrinsic {}
+        @must_use fn dpdyCoarse() @#intrinsic {}
+        @must_use fn dpdyFine() @#intrinsic {}
+        @must_use fn fwidth() @#intrinsic {}
+        @must_use fn fwidthCoarse() @#intrinsic {}
+        @must_use fn fwidthFine() @#intrinsic {}
 
         // texture
-        @must_use fn textureDimensions() @__intrinsic {}
-        @must_use fn textureGather() @__intrinsic {}
-        @must_use fn textureGatherCompare() @__intrinsic {}
-        @must_use fn textureLoad() @__intrinsic {}
-        @must_use fn textureNumLayers() @__intrinsic {}
-        @must_use fn textureNumLevels() @__intrinsic {}
-        @must_use fn textureNumSamples() @__intrinsic {}
-        @must_use fn textureSample() @__intrinsic {}
-        @must_use fn textureSampleBias() @__intrinsic {}
-        @must_use fn textureSampleCompare() @__intrinsic {}
-        @must_use fn textureSampleCompareLevel() @__intrinsic {}
-        @must_use fn textureSampleGrad() @__intrinsic {}
-        @must_use fn textureSampleLevel() @__intrinsic {}
-        @must_use fn textureSampleBaseClampToEdge() @__intrinsic {}
-        fn textureStore() @__intrinsic {}
+        @must_use fn textureDimensions() @#intrinsic {}
+        @must_use fn textureGather() @#intrinsic {}
+        @must_use fn textureGatherCompare() @#intrinsic {}
+        @must_use fn textureLoad() @#intrinsic {}
+        @must_use fn textureNumLayers() @#intrinsic {}
+        @must_use fn textureNumLevels() @#intrinsic {}
+        @must_use fn textureNumSamples() @#intrinsic {}
+        @must_use fn textureSample() @#intrinsic {}
+        @must_use fn textureSampleBias() @#intrinsic {}
+        @must_use fn textureSampleCompare() @#intrinsic {}
+        @must_use fn textureSampleCompareLevel() @#intrinsic {}
+        @must_use fn textureSampleGrad() @#intrinsic {}
+        @must_use fn textureSampleLevel() @#intrinsic {}
+        @must_use fn textureSampleBaseClampToEdge() @#intrinsic {}
+        fn textureStore() @#intrinsic {}
 
         // atomic
-        fn atomicLoad() @__intrinsic {}
-        fn atomicStore() @__intrinsic {}
-        fn atomicAdd() @__intrinsic {}
-        fn atomicSub() @__intrinsic {}
-        fn atomicMax() @__intrinsic {}
-        fn atomicMin() @__intrinsic {}
-        fn atomicAnd() @__intrinsic {}
-        fn atomicOr() @__intrinsic {}
-        fn atomicXor() @__intrinsic {}
-        fn atomicExchange() @__intrinsic {}
-        fn atomicCompareExchangeWeak() @__intrinsic {}
+        fn atomicLoad() @#intrinsic {}
+        fn atomicStore() @#intrinsic {}
+        fn atomicAdd() @#intrinsic {}
+        fn atomicSub() @#intrinsic {}
+        fn atomicMax() @#intrinsic {}
+        fn atomicMin() @#intrinsic {}
+        fn atomicAnd() @#intrinsic {}
+        fn atomicOr() @#intrinsic {}
+        fn atomicXor() @#intrinsic {}
+        fn atomicExchange() @#intrinsic {}
+        fn atomicCompareExchangeWeak() @#intrinsic {}
 
         // packing
-        @const @must_use fn pack4x8snorm() @__intrinsic { }
-        @const @must_use fn pack4x8unorm() @__intrinsic {}
-        @const @must_use fn pack4xI8() @__intrinsic {}
-        @const @must_use fn pack4xU8() @__intrinsic {}
-        @const @must_use fn pack4xI8Clamp() @__intrinsic {}
-        @const @must_use fn pack4xU8Clamp() @__intrinsic {}
-        @const @must_use fn pack2x16snorm() @__intrinsic {}
-        @const @must_use fn pack2x16unorm() @__intrinsic {}
-        @const @must_use fn pack2x16float() @__intrinsic {}
-        @const @must_use fn unpack4x8snorm() @__intrinsic {}
-        @const @must_use fn unpack4x8unorm() @__intrinsic {}
-        @const @must_use fn unpack4xI8() @__intrinsic {}
-        @const @must_use fn unpack4xU8() @__intrinsic {}
-        @const @must_use fn unpack2x16snorm() @__intrinsic {}
-        @const @must_use fn unpack2x16unorm() @__intrinsic {}
-        @const @must_use fn unpack2x16float() @__intrinsic {}
+        @const @must_use fn pack4x8snorm() @#intrinsic { }
+        @const @must_use fn pack4x8unorm() @#intrinsic {}
+        @const @must_use fn pack4xI8() @#intrinsic {}
+        @const @must_use fn pack4xU8() @#intrinsic {}
+        @const @must_use fn pack4xI8Clamp() @#intrinsic {}
+        @const @must_use fn pack4xU8Clamp() @#intrinsic {}
+        @const @must_use fn pack2x16snorm() @#intrinsic {}
+        @const @must_use fn pack2x16unorm() @#intrinsic {}
+        @const @must_use fn pack2x16float() @#intrinsic {}
+        @const @must_use fn unpack4x8snorm() @#intrinsic {}
+        @const @must_use fn unpack4x8unorm() @#intrinsic {}
+        @const @must_use fn unpack4xI8() @#intrinsic {}
+        @const @must_use fn unpack4xU8() @#intrinsic {}
+        @const @must_use fn unpack2x16snorm() @#intrinsic {}
+        @const @must_use fn unpack2x16unorm() @#intrinsic {}
+        @const @must_use fn unpack2x16float() @#intrinsic {}
 
         // synchronization
-        fn storageBarrier() @__intrinsic {}
-        fn textureBarrier() @__intrinsic {}
-        fn workgroupBarrier() @__intrinsic {}
-        @must_use fn workgroupUniformLoad() @__intrinsic {}
+        fn storageBarrier() @#intrinsic {}
+        fn textureBarrier() @#intrinsic {}
+        fn workgroupBarrier() @#intrinsic {}
+        @must_use fn workgroupUniformLoad() @#intrinsic {}
 
         // subgroup
-        @must_use fn subgroupAdd() @__intrinsic {}
-        @must_use fn subgroupExclusiveAdd() @__intrinsic {}
-        @must_use fn subgroupInclusiveAdd() @__intrinsic {}
-        @must_use fn subgroupAll() @__intrinsic {}
-        @must_use fn subgroupAnd() @__intrinsic {}
-        @must_use fn subgroupAny() @__intrinsic {}
-        @must_use fn subgroupBallot() @__intrinsic {}
-        @must_use fn subgroupBroadcast() @__intrinsic {}
-        @must_use fn subgroupBroadcastFirst() @__intrinsic {}
-        @must_use fn subgroupElect() @__intrinsic {}
-        @must_use fn subgroupMax() @__intrinsic {}
-        @must_use fn subgroupMin() @__intrinsic {}
-        @must_use fn subgroupMul() @__intrinsic {}
-        @must_use fn subgroupExclusiveMul() @__intrinsic {}
-        @must_use fn subgroupInclusiveMul() @__intrinsic {}
-        @must_use fn subgroupOr() @__intrinsic {}
-        @must_use fn subgroupShuffle() @__intrinsic {}
-        @must_use fn subgroupShuffleDown() @__intrinsic {}
-        @must_use fn subgroupShuffleUp() @__intrinsic {}
-        @must_use fn subgroupShuffleXor() @__intrinsic {}
-        @must_use fn subgroupXor() @__intrinsic {}
+        @must_use fn subgroupAdd() @#intrinsic {}
+        @must_use fn subgroupExclusiveAdd() @#intrinsic {}
+        @must_use fn subgroupInclusiveAdd() @#intrinsic {}
+        @must_use fn subgroupAll() @#intrinsic {}
+        @must_use fn subgroupAnd() @#intrinsic {}
+        @must_use fn subgroupAny() @#intrinsic {}
+        @must_use fn subgroupBallot() @#intrinsic {}
+        @must_use fn subgroupBroadcast() @#intrinsic {}
+        @must_use fn subgroupBroadcastFirst() @#intrinsic {}
+        @must_use fn subgroupElect() @#intrinsic {}
+        @must_use fn subgroupMax() @#intrinsic {}
+        @must_use fn subgroupMin() @#intrinsic {}
+        @must_use fn subgroupMul() @#intrinsic {}
+        @must_use fn subgroupExclusiveMul() @#intrinsic {}
+        @must_use fn subgroupInclusiveMul() @#intrinsic {}
+        @must_use fn subgroupOr() @#intrinsic {}
+        @must_use fn subgroupShuffle() @#intrinsic {}
+        @must_use fn subgroupShuffleDown() @#intrinsic {}
+        @must_use fn subgroupShuffleUp() @#intrinsic {}
+        @must_use fn subgroupShuffleXor() @#intrinsic {}
+        @must_use fn subgroupXor() @#intrinsic {}
 
         // quad
-        @must_use fn quadBroadcast() @__intrinsic {}
-        @must_use fn quadSwapDiagonal() @__intrinsic {}
-        @must_use fn quadSwapX() @__intrinsic {}
-        @must_use fn quadSwapY() @__intrinsic {}
+        @must_use fn quadBroadcast() @#intrinsic {}
+        @must_use fn quadSwapDiagonal() @#intrinsic {}
+        @must_use fn quadSwapX() @#intrinsic {}
+        @must_use fn quadSwapY() @#intrinsic {}
     }
 });
 
