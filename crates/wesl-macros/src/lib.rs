da3aaec9ae2606021ce7a3c7437ae6d358a1a2ae
Mathis Brossier <mathis.brossier@gmail.com>
2025-07-27 17:10:16 +0200

    feat flags for wesl-macros

--- crates/wesl-macros/src/lib.rs
+++ crates/wesl-macros/src/lib.rs
@@ -1,53 +1,66 @@
-use proc_macro_error2::proc_macro_error;
-use query_macro::{QueryInput, query_impl};
-use quote_macro::{QuoteNodeKind, quote_impl};
-use syn::parse_macro_input;
-
+#[cfg(feature = "query")]
 mod query_macro;
+#[cfg(feature = "quote")]
 mod quote_macro;
 
+use proc_macro_error2::proc_macro_error;
+use syn::parse_macro_input;
+
+#[cfg(feature = "query")]
+use query_macro::{QueryInput, query_impl};
+#[cfg(feature = "quote")]
+use quote_macro::{QuoteNodeKind, quote_impl};
+
+#[cfg(feature = "query")]
 #[proc_macro]
 pub fn query(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as QueryInput);
     query_impl(input, false).into()
 }
-
+#[cfg(feature = "query")]
 #[proc_macro]
 pub fn query_mut(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as QueryInput);
     query_impl(input, true).into()
 }
 
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_module(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::TranslationUnit, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_import(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::ImportStatement, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_declaration(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::GlobalDeclaration, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_literal(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::Literal, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_directive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::GlobalDirective, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_expression(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     quote_impl(QuoteNodeKind::Expression, input.into()).into()
 }
+#[cfg(feature = "quote")]
 #[proc_macro_error]
 #[proc_macro]
 pub fn quote_statement(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
